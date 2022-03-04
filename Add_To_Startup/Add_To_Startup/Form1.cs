41b5ad49251dd7f1e2efab475ec293f9d32cf9c9
Atalayagiz <78467521+Atalayagiz@users.noreply.github.com>
2022-03-04 20:58:26 +0300

    Add files via upload

---
+++ Add_To_Startup/Add_To_Startup/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+
+namespace Add_To_Startup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Adds an shortcut to Startup Folder
+        //C:\Users\Your Username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WshShell wshShell = new WshShell();
+            IWshRuntimeLibrary.IWshShortcut shortcut;
+            string startUpFolderPath =
+              Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+
+            // Create the shortcut
+            shortcut =
+              (IWshRuntimeLibrary.IWshShortcut)wshShell.CreateShortcut(
+                startUpFolderPath + "\\" +
+                Application.ProductName + ".lnk");
+
+            shortcut.TargetPath = Application.ExecutablePath;
+            shortcut.WorkingDirectory = Application.StartupPath;
+            shortcut.Description = "Launch My Application";
+            // shortcut.IconLocation = Application.StartupPath + @"\App.ico";
+            shortcut.Save();
+
+        }
+    }
+}
